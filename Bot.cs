5822d338eb14b7ae070fe5ae780417ff355bf565
aleatoreo22 <aleatoreo8@gmail.com>
2024-10-26 01:15:19 -0300

    fix: Correcao na passagem de parametros

--- Bot.cs
+++ Bot.cs
@@ -64,7 +64,9 @@ public class Bot
                 await e.Message.RespondAsync("Comando nao reconhecido! `'-'`");
                 return;
             }
-            var paramters = e.Message.Content[(comandText.Length + 2)..].Split(' ');
+            var paramters = Array.Empty<string>();
+            if (e.Message.Content.Length > comandText.Length + 2)
+                paramters = e.Message.Content[(comandText.Length + 2)..].Split(' ');
             paramters.ToList().RemoveAll(x => x == "");
             using var lua = new Lua();
             lua.DoFile(luaFile);
